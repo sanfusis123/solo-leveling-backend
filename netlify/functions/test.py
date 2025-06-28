6f2ec983514ac09cc71c80b5e14f64bad1154f31
sanfusis123 <sanfusis123@gmail.com>
2025-06-29 00:31:24 +0530

    netlify0.3

--- netlify/functions/test.py
+++
@@ -1,9 +0,0 @@
-def handler(event, context):
-    return {
-        'statusCode': 200,
-        'headers': {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        },
-        'body': '{"message": "Test function is working!", "path": "' + event.get('path', 'no path') + '"}'
-    }
